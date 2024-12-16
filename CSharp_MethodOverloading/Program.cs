63871038c6d5440ac315c03057f7b6be08f7e4b7
Nilesh Katkar <nilesh.katkar@xpressmoney.tech>
2024-12-16 12:06:47 +0530

    Dotnet Tutorials

---
+++ CSharp_MethodOverloading/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        int Sum;
+        program.Add(15,20,out Sum);
+
+        Console.WriteLine($"Sum = {Sum}");
+    }
+
+    public void Add(float FN, int SN, int TN)
+    {
+        Console.WriteLine("Sum = {Sum}", FN + SN);
+    }
+
+    public void Add(int FN, int SN, out int Sum)
+    {
+        Sum = FN + SN;
+        Console.WriteLine($"Sum = {Sum}", FN + SN);      
+    }
+
+    //public void Add(int FN, int SN)
+    //{
+    //    Console.WriteLine("Sum = {Sum}", FN + SN);
+    //}
+
+    //public void Add(float FN, int SN)
+    //{
+    //    Console.WriteLine("Sum = {Sum}", FN + SN);
+    //}
+
+    //public void Add(int FN, float SN)
+    //{
+    //    Console.WriteLine("Sum = {Sum}", FN + SN);
+    //}
+
+    //public void Add(float FN, float SN)
+    //{
+    //    Console.WriteLine("Sum = {Sum}", FN + SN);
+    //}
+}
