2b0d3eaa99c7e4f05ba0118235fb9bad0753a0f2
Nilesh Katkar <nilesh.katkar@xpressmoney.tech>
2024-12-16 18:53:27 +0530

    Dotnet Tutorials

---
+++ CSharp_DifferenceBtweenClassAndStruct/Program.cs
@@ -0,0 +1,55 @@
+﻿
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Customer() { }
+
+    ~Customer() { }
+}
+
+public struct Customer1
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    //public Customer1() { }    // Error - Struct cannot have explicit parameterless ctor
+
+    //~Customer() { }   // Error - Struct cannot have destructors
+}
+
+class Program
+{
+    public static void Main()
+    {
+        //Console.WriteLine("Welcome");
+
+        //System.Int32 i = 0;
+        //int i = 0;
+
+        //if (i == 10) 
+        //{
+        //    int j = 20;
+        //    Customer customer = new Customer();
+        //    customer.Id = 101;
+        //    customer.Name = "Nilesh";
+        //}
+
+        int i = 10;
+        int j = i;
+        j=j+1;
+
+        Console.WriteLine("i = {0} && j = {1}",i,j);
+
+        Customer customer = new Customer();
+        customer.Id = 101;
+        customer.Name = "Shivam";
+
+        Customer customer2 = customer;
+
+        customer2.Name = "Khushi";
+
+        Console.WriteLine("customer.Name = {0} && customer2.Name = {1}", customer.Name, customer2.Name);
+    }
+}
---
+++ CSharp_StructsInC#/Program.cs
@@ -0,0 +1,56 @@
+﻿
+public struct Customer
+{
+    private int _ID;
+
+    private string _Name;
+     
+    public int Id
+    {
+        get { return this._ID; }
+
+        set { this._ID = value; }
+    }
+
+    public string Name 
+    { 
+        get => _Name; 
+        set => _Name = value; 
+    }
+
+    public Customer(int ID, string Name)
+    {
+        this._ID = ID;
+        this._Name = Name;
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine("Id = {0}, Name = {1}", this._ID,this._Name);
+    }
+}
+
+
+class Program
+{
+    public static void Main()
+    {
+        //Console.WriteLine("Welcome to CSharp Tutorial");
+
+        Customer customer = new Customer(101, "Nilesh");
+        customer.PrintDetails();
+
+        Customer customer2 = new Customer();
+        customer2.Id = 102;
+        customer2.Name = "Vaibhav";
+        customer2.PrintDetails();
+
+        Customer customer3 = new Customer
+        {
+            Id = 103,
+            Name = "Shrilekh"
+        };
+
+        customer3.PrintDetails();
+    }
+}
